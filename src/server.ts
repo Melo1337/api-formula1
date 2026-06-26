30046b2a68adef4c540b5f33e9238dc9cef0e55b
Melo <102263606+Melo1337@users.noreply.github.com>
2026-06-25 22:11:15 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from "fastify";
 import jsonFormula from "./repository/formula-one.json"
+import cors from "@fastify/cors"
+
 const server = fastify({logger:true})
+
+server.register(cors, {
+    origin: "*"
+})
+
 
 server.get("/", async(req, res)=>{
     res.type("Application/json").code(200)
@@ -13,6 +20,22 @@ server.get("/pilots", async(req, res)=>{
 
     res.type("Application/json").code(200)
     return data
+})
+
+server.get("/equipes", async(req,res)=>{
+    const data = jsonFormula.map( time => time.equipe )
+    res.type("Application/json").code(200)
+    return data
+})
+
+server.get<{ Params: { search: string } }>("/:search", async (req, res) => {
+    const search = req.params.search
+    const searchResultEquip = jsonFormula.find((e) => e.equipe === search)
+    const searchResultDriver = jsonFormula.find((d) => d.pilotos.includes(search))
+    const searchResult = searchResultEquip ?? searchResultDriver
+
+    res.type("application/json").code(200)
+    return searchResult ?? { message: 'Not found' }
 })
 
 server.listen({port:3333},()=>{
